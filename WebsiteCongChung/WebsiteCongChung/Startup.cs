d07f348faa0d4e440e60fbbe7f1d5efdb755eb66
phucanhtran <49139939+phucanhtran@users.noreply.github.com>
2019-04-20 14:02:48 +0700

    Initialize Solution

---
+++ WebsiteCongChung/WebsiteCongChung/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteCongChung.Startup))]
+namespace WebsiteCongChung
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
